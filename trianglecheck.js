dbd2a55a2fda9b5bf2e5f94be1980acb52ee33b8
Nataliia Salamatina <nataliia.salamatina@gmail.com>
2022-09-25 17:07:00 +0200

    added styles

--- trianglecheck.js
+++ trianglecheck.js
@@ -1,10 +1,8 @@
-
 let submitBtn = (document.getElementById("fbtn").onclick = function () {
   let left = document.getElementById("left-value").value;
   let right = document.getElementById("right-value").value;
   let height = document.getElementById("height").value;
 
-  console.log(left, right, height);
   isTriangle(Number(left), Number(right), Number(height));
 });
 
@@ -12,12 +10,16 @@ function isTriangle(a, b, c) {
   let triangle = a + b > c && a + c > b && c + b > a;
 
   if (triangle) {
+    document.getElementById("text").innerHTML = "Here's your triangle:";
     document.getElementById(
       "triangle-representation"
     ).style.borderWidth = `0 ${b}rem ${c}rem ${a}rem`;
+    document.getElementById("triangle-representation").style.borderColor =
+      "transparent transparent red transparent";
   } else {
     document.getElementById("text").innerHTML =
       "Sorry, a triangle cannot be built with the sides of given length";
+    document.getElementById("triangle-representation").style.borderColor =
+      "transparent";
   }
 }
-
