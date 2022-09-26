5489ee61ce726a6a56c2d047f39ba9f12ad22af8
Nataliia Salamatina <nataliia.salamatina@gmail.com>
2022-09-26 20:04:49 +0200

    build triangle based on side length

--- trianglecheck.js
+++ trianglecheck.js
@@ -1,19 +1,22 @@
 let submitBtn = (document.getElementById("fbtn").onclick = function () {
   let left = document.getElementById("left-value").value;
   let right = document.getElementById("right-value").value;
-  let height = document.getElementById("height").value;
+  let side = document.getElementById("height").value;
 
-  isTriangle(Number(left), Number(right), Number(height));
+  isTriangle(Number(left), Number(right), Number(side));
 });
 
 function isTriangle(a, b, c) {
   let triangle = a + b > c && a + c > b && c + b > a;
 
   if (triangle) {
+      let p = (a + b + c) / 2;
+    let height = (a * Math.sqrt(p * (p - a) * (p - b) * (p - c))) / a;
+    console.log(height)
     document.getElementById("text").innerHTML = "Here's your triangle:";
     document.getElementById(
       "triangle-representation"
-    ).style.borderWidth = `0 ${b}rem ${c}rem ${a}rem`;
+    ).style.borderWidth = `0 ${b}rem ${height}rem ${a}rem`;
     document.getElementById("triangle-representation").style.borderColor =
       "transparent transparent red transparent";
   } else {
